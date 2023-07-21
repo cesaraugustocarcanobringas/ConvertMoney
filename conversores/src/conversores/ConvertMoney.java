4d836fcc1acf6d232fd99f2ba2c9f7a91bb84a27
Cesar Carcano <azul45cesar@gmail.com>
2023-07-20 23:38:53 -0600

    Se Hizo el menu Monedas

--- conversores/src/conversores/ConvertMoney.java
+++ conversores/src/conversores/ConvertMoney.java
@@ -1,5 +1,7 @@
 package conversores;
 
+import javax.swing.JOptionPane;
+
 public class ConvertMoney {
 	public static void main(String[] args) {
 		
@@ -10,6 +12,25 @@ Double conversion;
 Double resultado;
 Double temperatura;
 Double redondeo;
+
+do {
+	String conversiones = (JOptionPane.showInputDialog(null, "Seleccione una opcion de conversion :","Conversiones de Convert Money",
+			JOptionPane.PLAIN_MESSAGE,null,new Object[] {"MONEDAS", "TEMPERATURA"},
+			"Selecciona")).toString();		
+
+	if ("MONEDAS".equals(conversiones)) {
+		pesos = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad que desea convertir :"));
+		String numero = (JOptionPane.showInputDialog(null,"A que moneda deseas convertirlo :","Moneda",
+				JOptionPane.PLAIN_MESSAGE,null,new Object[] {"De Pesos a Dolar", "De Pesos a Euros", "De Pesos a Libras Esterlinas", "De Pesos a Yen Japones", "De Pesos a Won Surcoreano",
+															 "De Dolar a Pesos", "De Euros a Pesos", "De Libras Esterlinas a Pesos", "De Yen Japones a Pesos", "De Won Surcoreano a Pesos"},
+															 "Selecciona")).toString();
+			
+		
+	}	//monedas
+	
+	
+	
+}while(salida <1);
 
 
 		
--- conversores/src/module-info.java
+++ conversores/src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module conversores {
+	requires java.desktop;
 }
