621bd9af460f5abfebc89d8ec93c7b8e5c9fe576
Cesar Carcano <azul45cesar@gmail.com>
2023-07-21 00:18:30 -0600

    Se termino el menu Monedas con sus respectivas operaciones

--- conversores/src/conversores/ConvertMoney.java
+++ conversores/src/conversores/ConvertMoney.java
@@ -24,7 +24,71 @@ do {
 				JOptionPane.PLAIN_MESSAGE,null,new Object[] {"De Pesos a Dolar", "De Pesos a Euros", "De Pesos a Libras Esterlinas", "De Pesos a Yen Japones", "De Pesos a Won Surcoreano",
 															 "De Dolar a Pesos", "De Euros a Pesos", "De Libras Esterlinas a Pesos", "De Yen Japones a Pesos", "De Won Surcoreano a Pesos"},
 															 "Selecciona")).toString();
+	
+		if ("De Pesos a Dolar".equals(numero)) {
+			conversion = 16.76;
+			resultado = pesos/conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Dolares");
+		}
+		if ("De Pesos a Euros".equals(numero)) {
+			conversion = 18.84;
+			resultado = pesos/conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Euros");
+		}
+		if ("De Pesos a Libras Esterlinas".equals(numero)) {
+			conversion = 21.92;
+			resultado = pesos/conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Libras Esterlinas");
+		}
+		if ("De Pesos a Yen Japones".equals(numero)) {
+			conversion = 8.28;
+			resultado = pesos*conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Yenes");
+		}
+		if ("De Pesos a Won Surcoreano".equals(numero)) {
+			conversion = 75.48;
+			resultado = pesos*conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Wones");
+		}
+		if ("De Dolar a Pesos".equals(numero)) {
+			conversion = 16.76;
+			resultado = pesos*conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Pesos");
+		}
+		if ("De Euros a Pesos".equals(numero)) {
+			conversion = 18.84;
+			resultado = pesos*conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Pesos");
+		}
+		if ("De Libras Esterlinas a Pesos".equals(numero)) {
+			conversion =21.92;
+			resultado = pesos*conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Pesos");
+		}
+		if ("De Yen Japones a Pesos".equals(numero)) {
+			conversion =.12;
+			resultado = pesos*conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Pesos");
+		}
+		if ("De Won Surcoreano a Pesos".equals(numero)) {
+			conversion =.013;
+			resultado = pesos*conversion;
+			JOptionPane.showMessageDialog(null,"Tienes $"+String.format("%.2f", resultado) +" Pesos");
+		}else {
+			salida =JOptionPane.showConfirmDialog(null," Alguna otra operacion ?");	
+			switch(salida) {
 			
+				case 1:
+				JOptionPane.showMessageDialog(null,"Programa Finalizado ");
+				break;
+			    case 2:
+				JOptionPane.showMessageDialog(null,"Programa Finalizado ");
+				break;
+				
+			}			
+			
+		}
+		
 		
 	}	//monedas
 	
